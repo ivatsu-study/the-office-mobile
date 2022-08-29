e746c2720305dca766d21c4d31fc9604d894ec62
IvaTsu <ivan.tsud@1337.tech>
2022-08-29 15:14:16 +0200

    feat: create main screen skeleton

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>
+          Quotes, facts and information on NBC’s beloved mockumentary series The
+          Office.
+        </Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Quotes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Characters</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Episodes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Crew</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

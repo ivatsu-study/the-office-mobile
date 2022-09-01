0ee6b23744fed4b08ab9e5323f25d8cc7a669f2e
IvaTsu <ivan.tsud@1337.tech>
2022-09-01 13:38:40 +0200

    style: add basic styles for 'Quotes Screen'

---
+++ src/screens/QuotesScreen/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    margin: 12,
+  },
+  quoteContainer: {
+    marginVertical: 12,
+    padding: 12,
+    backgroundColor: '#BFC3C6',
+  },
+  quoteText: {
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  quouteAuthor: {
+    marginVertical: 12,
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'right',
+  },
+});
+
+export default styles;

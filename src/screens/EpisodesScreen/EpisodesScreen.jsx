90b8bb37a9d94a22e66f7a2997c9a217b208e4ce
IvaTsu <ivan.tsud@1337.tech>
2022-09-02 14:48:34 +0200

    style: add basic layout for 'EpisodesScreen'

--- src/screens/EpisodesScreen/EpisodesScreen.jsx
+++ src/screens/EpisodesScreen/EpisodesScreen.jsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 import { GET_EPISODES } from '../../api/url';
 import { AppContainer, ScrollViewContainer } from '../../components/shared';
 
+import styles from './styles';
+
 function EpisodesScreen() {
   const { isLoading, error, data } = useQuery(['episodesData'], () => fetch(GET_EPISODES).then((res) => res.json()));
   if (isLoading) return <Text>Loading...</Text>;
@@ -17,18 +19,18 @@ function EpisodesScreen() {
     <AppContainer withStatusBar>
       <ScrollViewContainer>
         {data?.data.map((episode) => (
-          <View key={episode._id}>
-            <Text>{episode.title}</Text>
-            <Text>{episode.description}</Text>
-            {episode.director.firstname && episode.director.lastname && (
-              <Text>{`${episode.director.firstname} ${episode.director.lastname}`}</Text>
-            )}
-            <Text>
-              {episode.writer.name}
-              {' '}
-              {`(${episode.writer.role})`}
-            </Text>
-            <Text>{episode.airDate}</Text>
+          <View key={episode._id} style={styles.episodeContainer}>
+            <Text style={styles.episodeTitle}>{episode.title}</Text>
+            <Text style={styles.episodeAirDate}>{`${new Date(episode.airDate).getUTCDate()}/${new Date(episode.airDate).getMonth()}/${new Date(episode.airDate).getFullYear()}`}</Text>
+            <Text style={styles.episodeDescription}>{episode.description}</Text>
+            <View style={styles.episodeDirector}>
+              <Text>{`${episode.director.name} (${episode.director.role})`}</Text>
+              <Text>
+                {episode.writer.name}
+                {' '}
+                {`(${episode.writer.role})`}
+              </Text>
+            </View>
           </View>
         ))}
       </ScrollViewContainer>
---
+++ src/screens/EpisodesScreen/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  episodeContainer: {
+    margin: 12,
+    backgroundColor: '#BFC3C6',
+  },
+  episodeTitle: {
+    marginVertical: 12,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  episodeAirDate: {
+    textAlign: 'center',
+  },
+  episodeDescription: {
+    margin: 12,
+    textAlign: 'justify',
+    fontSize: 16,
+  },
+  episodeDirector: {
+    margin: 12,
+  },
+});
+
+export default styles;

fdf262820006b6d7ca71a73b9cda13ad1e0aa420
IvaTsu <ivan.tsud@1337.tech>
2023-01-05 10:16:23 +0100

    refactor(React-Query): wrap App into QueryProvider, clean NavigationStack

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NavigationStack from './src/navigation/NavigationStack'
 
-const App: React.FunctionComponent = () => <NavigationStack />
+const queryClient = new QueryClient()
+
+const App: React.FunctionComponent = () => (
+  <QueryClientProvider client={queryClient}>
+    <NavigationStack />
+  </QueryClientProvider>
+)
 
 export default App
--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import {
   CharactersScreen,
@@ -20,26 +19,20 @@ import {
 import { IRootStackParamList } from './types'
 
 const Stack = createNativeStackNavigator<IRootStackParamList>()
-const queryClient = new QueryClient()
 
 const NavigationStack: React.FunctionComponent = () => {
   return (
     <NavigationContainer>
-      <QueryClientProvider client={queryClient}>
-        <Stack.Navigator initialRouteName={HOME_SCREEN_NAME}>
-          <Stack.Screen name={HOME_SCREEN_NAME} component={HomeScreen} />
-          <Stack.Screen name={QUOTES_SCREEN_NAME} component={QuotesScreen} />
-          <Stack.Screen
-            name={CHARACTERS_SCREEN_NAME}
-            component={CharactersScreen}
-          />
-          <Stack.Screen
-            name={EPISODES_SCREEN_NAME}
-            component={EpisodesScreen}
-          />
-          <Stack.Screen name={CREW_SCREEN_NAME} component={CrewScreen} />
-        </Stack.Navigator>
-      </QueryClientProvider>
+      <Stack.Navigator initialRouteName={HOME_SCREEN_NAME}>
+        <Stack.Screen name={HOME_SCREEN_NAME} component={HomeScreen} />
+        <Stack.Screen name={QUOTES_SCREEN_NAME} component={QuotesScreen} />
+        <Stack.Screen
+          name={CHARACTERS_SCREEN_NAME}
+          component={CharactersScreen}
+        />
+        <Stack.Screen name={EPISODES_SCREEN_NAME} component={EpisodesScreen} />
+        <Stack.Screen name={CREW_SCREEN_NAME} component={CrewScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
